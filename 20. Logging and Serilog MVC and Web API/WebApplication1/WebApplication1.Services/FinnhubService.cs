1de9b710518142347c2e3393258e88ba888475e1
ARTYO\artyo <artyom.shashok@mail.ru>
2026-05-23 00:32:38 +0300

    Added 20. Logging and Serilog MVC and Web API

---
+++ 20. Logging and Serilog MVC and Web API/WebApplication1/WebApplication1.Services/FinnhubService.cs
@@ -0,0 +1,38 @@
+﻿using WebApplication1.RepositoryContracts;
+using WebApplication1.ServiceContracts;
+
+namespace WebApplication1.Services
+{
+ public class FinnhubService : IFinnhubService
+ {
+  private readonly IFinnhubRepository _finnhubRepository;
+  public FinnhubService(IFinnhubRepository finnhubRepository)
+  {
+   _finnhubRepository = finnhubRepository;
+  }
+
+  public async Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+  {
+   Dictionary<string, object>? responseDictionary = await _finnhubRepository.GetCompanyProfile(stockSymbol);
+   return responseDictionary;
+  }
+
+  public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+  {
+   Dictionary<string, object>? responseDictionary = await _finnhubRepository.GetStockPriceQuote(stockSymbol);
+   return responseDictionary;
+  }
+
+  public async Task<List<Dictionary<string, string>>?> GetStocks()
+  {
+   List<Dictionary<string, string>>? responseDictionary = await _finnhubRepository.GetStocks();
+   return responseDictionary;
+  }
+
+  public async Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch)
+  {
+   Dictionary<string, object>? responseDictionary = await _finnhubRepository.SearchStocks(stockSymbolToSearch);
+   return responseDictionary;
+  }
+ }
+}
---
+++ 20. Logging and Serilog MVC and Web API/WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+using WebApplication1.DataAccess;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+using WebApplication1.RepositoryContracts;
+using WebApplication1.ServiceContracts;
+using WebApplication1.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, loggerConfiguration) => {
+
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services);
+});
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<IStocksService, StocksService>();
+builder.Services.AddScoped<IFinnhubService, FinnhubService>();
+builder.Services.AddScoped<IStocksRepository, StocksRepository>();
+builder.Services.AddScoped<IFinnhubRepository, FinnhubRepository>();
+
+var app = builder.Build();
+
+if (builder.Environment.IsEnvironment("Test") == false)
+    RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
+
+app.UseSerilogRequestLogging();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { } //make the auto-generated Program accessible programmatically
